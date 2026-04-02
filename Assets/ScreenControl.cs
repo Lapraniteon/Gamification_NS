934085c260e8d2c34088ed59e147c2119f206d76
Lapraniteon <lapraniteon@gmail.com>
2026-04-02 15:21:27 +0200

    Menu buttons

---
+++ Assets/ScreenControl.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenControl : MonoBehaviour
+{
+    
+    public void ChangeScreen(string screenName)
+    {
+        foreach (RectTransform child in GetComponent<RectTransform>())
+        {
+            Debug.Log(child.gameObject.name);
+            child.gameObject.SetActive(screenName == child.gameObject.name);
+        }
+    }
+}
