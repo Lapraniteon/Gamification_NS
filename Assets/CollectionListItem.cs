13fa1df8dac0f18864f1721d6ecad4eea99aab5e
Lapraniteon <lapraniteon@gmail.com>
2026-05-05 10:34:56 +0200

    Implement CollectionListItem functionality

---
+++ Assets/CollectionListItem.cs
@@ -0,0 +1,30 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollectionListItem : MonoBehaviour
+{
+    [SerializeField] private Slider progressBar;
+
+    [SerializeField] private TMP_Text percentageText;
+    [SerializeField] private TMP_Text fractionText;
+
+    private void Start()
+    {
+        OnBarValueChanged(progressBar.value);
+    }
+    
+    public void ChangeProgress(float progress)
+    {
+        progressBar.value += progress;
+        OnBarValueChanged(progressBar.value);
+    }
+
+    private void OnBarValueChanged(float value)
+    {
+        int percentage = (int)(value / progressBar.maxValue * 100);
+        percentageText.text = percentage + "%";
+
+        fractionText.text = $"{value}/{progressBar.maxValue}";
+    }
+}
